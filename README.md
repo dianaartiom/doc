22a8e50666674ba6649261186c2e50311bb62c11
Diana ARTIOM <diana.artiom24@gmail.com>
2017-11-20 13:49:41 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+###Scope
+This laboratory stands for ipp labs.
+
+The developed app will be a booking app wor PBLM rooms.
---
+++ src/test/java/com/example/reserver/ReserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.reserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
